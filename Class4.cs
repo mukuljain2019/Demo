e78bb0284e084d6dc41bbacf9dc8634928722c2e
mukul <mukul.jain011@gmail.com>
2019-06-15 14:53:01 -0700

    My first commited project

---
+++ Class4.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Class4
+    {
+        public int method()
+        {
+            return (2);
+            Console.WriteLine("I am in method1");
+        }
+        public static void main()
+        {
+            int res;
+            Class4 ob = new Class4();
+            res = ob.method();
+            Console.WriteLine("res="+res);
+        }
+    }
+}
